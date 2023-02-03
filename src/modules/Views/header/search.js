7c450bf51df795b23d0b2a33bd32db805c87af49
bitfrontinfotech <pk.bitfrontinfotech@gmail.com>
2023-02-03 21:21:45 +0530

    working on search

--- src/modules/Views/header/search.js
+++ src/modules/Views/header/search.js
@@ -1,25 +1,85 @@
 import "./style.css";
 import { SearchLogo } from "../../../assets/headerLogo";
-import { TextField } from "@mui/material";
+import { Autocomplete, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+// import usePrevious from "../../hooks/UsePrevious";
 
 export default function Search() {
+  const [searchQuery, setSearchQuery] = useState("1");
+  const [queryInterval, setQueryInterval] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [page, setPage] = useState(1);
+  const limit = 5;
+  const loading = open && options.length === 0;
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  useEffect(() => {
+    // const prevQuery = usePrevious(searchQuery);
+    clearTimeout(queryInterval);
+    // console.log("prevQuery", prevQuery);
+
+    const interval = setTimeout(async () => {
+      const data = await fetchDataByQuery(searchQuery);
+      console.log("data", data);
+    }, 1000);
+    setQueryInterval(interval);
+  }, [searchQuery]);
+
+  const fetchDataByQuery = async (query) => {
+    try {
+      return await fetch(
+        `'https://jsonplaceholder.typicode.com/posts?userId=${query}'`
+      ).then((response) => response.json());
+    } catch (error) {
+      console.error("[failed at fetchDataByQuery]", { error });
+    }
+  };
+
   return (
     <>
-      <TextField
-        placeholder="Search Facebook"
+      <Autocomplete
+        className="autocomplete-search-wrapper"
+        id="autocomplete-search"
         fullWidth
-        className="facebook-search-field"
-        variant="outlined"
-        id="outlined-basic"
-        size="small"
-        InputProps={{
-          startAdornment: (
-            <div className="facebooke-seach-icon">
-              <SearchLogo />
-            </div>
-          ),
+        open={open}
+        options={options}
+        popupIcon={null}
+        getOptionLabel={(option) => option.year + " - " + option.title}
+        filterOptions={(x) => x}
+        loading={loading}
+        noOptionsText="No result found!"
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            value={searchQuery}
+            size="small"
+            className="facebook-search-field"
+            sx={{ outlineColor: "transparent" }}
+            InputProps={{
+              ...params.InputProps,
+              startAdornment: (
+                <div className="facebooke-seach-icon">
+                  <SearchLogo />
+                </div>
+              ),
+            }}
+            onChange={(event) => {
+              setSearchQuery(event.target.value);
+            }}
+          />
+        )}
+        onOpen={() => {
+          setOpen(true);
         }}
-        sx={{ outlineColor: "transparent" }}
+        onClose={() => {
+          setOpen(false);
+        }}
       />
     </>
   );
--- src/modules/Views/home/CreateStoryTab.js
+++ src/modules/Views/home/CreateStoryTab.js
@@ -1,4 +1,5 @@
-import { Button, Card, Typography } from "@mui/material";
+import { Button, Card, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { Component } from "react";
 
 class CreateStoryTab extends Component {
@@ -8,23 +9,38 @@ class CreateStoryTab extends Component {
   render() {
     return (
       <>
-        <Card sx={{ width: "15%", height: "150px", borderRadius: "12px" }}>
-          <Typography variant="subtitle2" textAlign={"center"}>
-            Create Story
-            <Button
-              variant="outlined"
-              sx={{
-                borderRadius: "50%",
-                width: 25,
-                height: 25,
-                minWidth: 20,
-              }}
-              onClick={this.props.onClick}
-            >
-              +
-            </Button>
+        <Box display='flex'>
+        <Grid width="15%">
+          <Card sx={{ width: "100%", height: "150px", borderRadius: "12px" }}>
+            <Typography variant="subtitle2" textAlign={"center"}>
+              Create Story
+              <Button
+                variant="outlined"
+                sx={{
+                  borderRadius: "50%",
+                  width: 25,
+                  height: 25,
+                  minWidth: 20,
+                }}
+                onClick={this.props.onClick}
+              >
+                +
+              </Button>
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid width="85%">
+          <Typography variant="caption">
+            Share everyday moments with friends and family.
           </Typography>
-        </Card>
+          <Typography variant="caption">
+            Stories disappear after 24 hours.
+          </Typography>
+          <Typography variant="caption">
+            Replies and reactions are private.
+          </Typography>
+        </Grid>
+        </Box>
       </>
     );
   }
